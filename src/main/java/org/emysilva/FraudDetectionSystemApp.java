5e25908dd1ecc00f2711d8193f8d973a9f1c94f9
RELIANCE HEALTH <reliancehealth@RELIANCEs-MacBook-Pro.local>
2024-03-15 17:00:57 +0100

    real time fraud detection system implementation

--- src/main/java/org/emysilva/FraudDetectionSystemApp.java
+++ src/main/java/org/emysilva/FraudDetectionSystemApp.java
@@ -7,15 +7,16 @@ import java.io.*;
 import java.time.Instant;
 
 public class FraudDetectionSystemApp {
+
+    private static final String FILE_NAME = "src/main/resources/dataset.txt";
+
     public static void main(String[] args) {
 
         FraudDetectionSystem fraudDetectionSystem = new FraudDetectionSystem();
 
         // Simulate transaction stream (replace with actual stream processing)
         // read data set from file
-        String fileName = "src/main/resources/dataset.txt";
-
-        try (FileInputStream fis = new FileInputStream(fileName);
+        try (FileInputStream fis = new FileInputStream(FILE_NAME);
              InputStreamReader isr = new InputStreamReader(fis);
              BufferedReader br = new BufferedReader(isr)) {
 
@@ -29,25 +30,5 @@ public class FraudDetectionSystemApp {
             System.out.println("An IO error occurred while reading file " + e.getMessage());
         }
 
-
-        // Simulate transaction stream (replace with actual stream processing)
-        // Example transaction events
-//        TransactionEvent[] transactionEvents = {
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 150.00, "user1", "serviceA"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 4500.00, "user2", "serviceB"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 75.00, "user1", "serviceC"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 3000.00, "user3", "serviceA"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 200.00, "user1", "serviceB"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 4800.00, "user2", "serviceC"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 100.00, "user4", "serviceA"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 4900.00, "user3", "serviceB"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 120.00, "user1", "serviceD"),
-//                new TransactionEvent(Instant.ofEpochSecond(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), 5000.00, "user3", "serviceC"),
-//        };
-//
-//        // Process each transaction event
-//        for (TransactionEvent event : transactionEvents) {
-//            fraudDetectionSystem.processTransaction(event);
-//        }
     }
 }
